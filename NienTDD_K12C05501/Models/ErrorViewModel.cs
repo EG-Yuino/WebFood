c774f04630a0f8b42ec3b2166b1c440ea87d695b
Yuino <yuino@pop-os.localdomain>
2022-06-08 21:04:53 +0700

    Feat: Initial commit

---
+++ NienTDD_K12C05501/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NienTDD_K12C05501.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
